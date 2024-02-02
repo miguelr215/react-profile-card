51e89bc219a028c610856fbef4fff80bee781588
miguelr215 <drexelramos@gmail.com>
2024-02-02 09:45:55 -0500

    profile card

--- src/App.js
+++ src/App.js
@@ -1,23 +1,62 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Card />
+    </div>
+  );
+}
+
+function ProfilePic(props) {
+  return (
+    <div className="profile-pic">
+      <img src={props.imgUrl} alt={props.name} />
+    </div>
+  );
+}
+
+function CardDetails(props) {
+  return (
+    <div className="details">
+      <h2>{props.name}</h2>
+      <p>{props.description}</p>
+    </div>
+  );
+}
+
+function SkillList() {
+  return (
+    <div className="skills-container">
+      <Skill skill="HTML" emoji="👌" bgColor="teal" />
+      <Skill skill="CSS" emoji="✔️" bgColor="orange" />
+      <Skill skill="JavaScript" emoji="👍🏽" bgColor="yellow" />
+      <Skill skill="React" emoji="💥" bgColor="skyblue" />
+      <Skill skill="Communication" emoji="📞" bgColor="silver" />
+    </div>
+  );
+}
+
+function Skill(props) {
+  return (
+    <div className="skill" style={{ background: props.bgColor }}>
+      <span>{props.skill}</span> <span>{props.emoji}</span>
+    </div>
+  );
+}
+
+function Card() {
+  return (
+    <div className="card">
+      <ProfilePic
+        name="Samantha"
+        imgUrl="https://randomuser.me/api/portraits/women/95.jpg"
+      />
+      <CardDetails
+        name="Samantha"
+        description="Works hard, plays hard! Philadelphia native, pizza and cheesesteak conniseur. Always focused on the goal of getting the job done."
+      />
+      <SkillList />
     </div>
   );
 }
